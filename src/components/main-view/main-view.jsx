a38358ea9d06b026f30e53e4a703c12d5d65dbe4
Damian Spears <ryanspears97@gmail.com>
2023-08-02 13:08:37 -0400

    Rendering data through components
    This branch will use the movie-api repo in order to load external data, removing the hardcode from the main branch.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,42 +5,9 @@ import { MovieView } from "../movie-view/movie-view"
 
 //This is the first 'MainView' component
 export const MainView = () => {     //The 'export' keyword exposes MainView so it can be used by other components. It is assigned a function that returns the code within
-   const [movies, setMovies] = useState([      //useState always takes two values, the first is the current 'state' and the second is the function to update the state
-   {
-      id: 1,
-      title: "Tenet",
-      director: "Christopher Nolan",
-      image: "https://upload.wikimedia.org/wikipedia/en/1/14/Tenet_movie_poster.jpg",
-      genre: [
-            {
-                Style: "Action",
-                Description: "fast-paced and include a lot of action like fight scenes, chase scenes, and slow-motion shots. They can feature superheroes, martial arts, or exciting stunts.",
-            }]
-    },
-    {
-      id: 2,
-      title: "Inception",
-      director: "Chrisopher Nolan",
-      image: "https://c8.alamy.com/comp/2C4X083/inception-2010-directed-by-christopher-nolan-and-starring-leonardo-dicaprio-joseph-gordon-levitt-ellen-page-tom-hardy-and-ken-watanabe-a-team-break-in-to-the-subconscious-of-a-businessman-using-dream-sharing-technology-in-order-a-plant-a-seed-to-influence-his-decision-in-the-real-world-2C4X083.jpg",
-      genre: [
-            {
-                style: "Action",
-                description: "fast-paced and include a lot of action like fight scenes, chase scenes, and slow-motion shots. They can feature superheroes, martial arts, or exciting stunts.",
-            }]
-    },
-    {
-      id: "64c161716290f044db676512",
-        title: "Reservoir Dogs",
-        director: "Quentin Tarantino",
-        image: "https://target.scene7.com/is/image/Target/GUEST_04cb340e-b760-456a-8252-35449ef3fda8?wid=488&hei=488&fmt=pjpeg",
-        genre: [
-            {
-                style: "Crime",
-                description: "Most crime drama focuses on crime investigation and does not feature the courtroom. Suspense and mystery are key elements that are nearly ubiquitous to the genre.",
-            }]
-    },
-   ]);   //the 'useState' hook allows you to create and initialize a new state for a component
-   //useState returns destructured values to be used as variables. 'movies' is the array of movies, and 'setmovies' is a method that updates the 'movies' array
+   const [movies, setMovies] = useState([]);    //useState always takes two values, the first is the current 'state' and the second is the function to update the state
+      //the 'useState' hook allows you to create and initialize a new state for a component
+      //useState returns destructured values to be used as variables. 'movies' is the array of movies, and 'setmovies' is a method that updates the 'movies' array
 
    const [selectedMovie, setSelectedMovie] = useState(null); //Created second state variable to display the MovieView component upon a 'click'
 
@@ -49,9 +16,27 @@ export const MainView = () => {     //The 'export' keyword exposes MainView so i
       />);  //When onBackClick fires, it changes etselectedMovie back to null
    }
 
-   if (movies.length === 0) {     //currently, the 'movies' current state is the movies listed above
+   if (movies.length === 0) {
       return <div>The list is empty</div>
    }
+
+   useEffect(() => {
+      fetch("https://pure-anchorage-69426-fa1526ceaf46.herokuapp.com/")   //fetch() returns a promise and needs a callback 'then()' in order to access it
+      .then((response) => response.json())
+      .then((data) => {
+         const moviesFromApi = data.docs.map((doc) => {  //data.docs.map searches through the API to return values for the defined keys in the return object below
+            return {
+               id: doc.id,
+               title: doc.title,
+               director: doc.director,
+               genre: [
+                  doc.genre.style,
+                  doc.genre.description
+            ]
+            }
+         });
+         setMovies(moviesFromApi);
+      })}, []);       //<-- passing this empty array '[]' at the end tells useEffect that the callback does not depend on changes in states or props
 
    return (
       <div>
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,25 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
-   return <div onClick={() => {onMovieClick(movie);   //the onmovieClick(movie) will send data from any clicked title and pass it to the function in the main view
+   return ( <div onClick={() => {onMovieClick(movie);   //the onmovieClick(movie) will send data from any clicked title and pass it to the function in the main view
    }}      //onmovieCLick allows each 'movie.title' to have a clickable option in order to change setSelectedmovie to newSelectedmovie
    >
-      {movie.title}</div>
+      {movie.title}
+   </div>
+   );
 };
 //The movieCard now iterates through each item and adds a click option, which presents the specififc information for that movie
+
+//Defining prop constraints for MovieCard below (what it should look like)
+BookCard.propTypes = {
+   book: PropTypes.shape({
+     //.shape is used to determine how the prop is supposed to look
+     title: PropTypes.string.isRequired,
+     director: PropTypes.string.isRequired,
+     genre: PropTypes.shape({
+      style: PropTypes.string,
+      description: PropTypes.string
+   })
+   }).isRequired,
+   onBookClick: PropTypes.func.isRequired
+ };
